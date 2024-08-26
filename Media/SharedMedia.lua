156c55eb583e213924c1bf8d7d0fe012b8122471
Mike Norton <mike@corcra.dev>
2024-08-25 20:10:10 -0700

    Updating to version 1.0.5

--- Media/SharedMedia.lua
+++ Media/SharedMedia.lua
@@ -1,33 +1,49 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
-if LSM == nil then return end
+if LSM == nil then
+    return
+end
 
 -- Montserrat
-LSM:Register("font", "Montserrat-Black",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Black.ttf]])
-LSM:Register("font", "Montserrat-Bold",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Bold.ttf]])
-LSM:Register("font", "Montserrat-Medium",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Medium.ttf]])
-LSM:Register("font", "Montserrat-Regular",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Regular.ttf]])
-LSM:Register("font", "Montserrat-SemiBold",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-SemiBold.ttf]])
+LSM:Register("font", "Montserrat-Black", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Black.ttf]])
+LSM:Register("font", "Montserrat-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Bold.ttf]])
+LSM:Register("font", "Montserrat-Medium", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Medium.ttf]])
+LSM:Register("font", "Montserrat-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-Regular.ttf]])
+LSM:Register("font", "Montserrat-SemiBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Montserrat-SemiBold.ttf]])
 
 -- Roboto
-LSM:Register("font", "Roboto-Black",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Black.ttf]])
-LSM:Register("font", "Roboto-Bold",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Bold.ttf]])
-LSM:Register("font", "Roboto-Medium",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Medium.ttf]])
-LSM:Register("font", "Roboto-Regular",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Regular.ttf]])
+LSM:Register("font", "Roboto-Black", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Black.ttf]])
+LSM:Register("font", "Roboto-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Bold.ttf]])
+LSM:Register("font", "Roboto-Medium", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Medium.ttf]])
+LSM:Register("font", "Roboto-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Roboto-Regular.ttf]])
 
 -- Lato
-LSM:Register("font", "Lato-Black",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Black.ttf]])
-LSM:Register("font", "Lato-Bold",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Bold.ttf]])
-LSM:Register("font", "Lato-Regular",
-             [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Regular.ttf]])
+LSM:Register("font", "Lato-Black", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Black.ttf]])
+LSM:Register("font", "Lato-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Bold.ttf]])
+LSM:Register("font", "Lato-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Lato-Regular.ttf]])
+
+-- Dosis
+LSM:Register("font", "Dosis-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-Bold.ttf]])
+LSM:Register("font", "Dosis-ExtraBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-ExtraBold.ttf]])
+LSM:Register("font", "Dosis-ExtraLight", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-ExtraLight.ttf]])
+LSM:Register("font", "Dosis-Light", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-Light.ttf]])
+LSM:Register("font", "Dosis-Medium", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-Medium.ttf]])
+LSM:Register("font", "Dosis-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-Regular.ttf]])
+LSM:Register("font", "Dosis-SemiBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Dosis-SemiBold.ttf]])
+
+-- Exo2
+LSM:Register("font", "Exo2-Black", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-Black.ttf]])
+LSM:Register("font", "Exo2-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-Bold.ttf]])
+LSM:Register("font", "Exo2-ExtraBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-ExtraBold.ttf]])
+LSM:Register("font", "Exo2-Medium", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-Medium.ttf]])
+LSM:Register("font", "Exo2-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-Regular.ttf]])
+LSM:Register("font", "Exo2-SemiBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Exo2-SemiBold.ttf]])
+
+-- Mukta
+LSM:Register("font", "Mukta-Bold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-Bold.ttf]])
+LSM:Register("font", "Mukta-ExtraBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-ExtraBold.ttf]])
+LSM:Register("font", "Mukta-ExtraLight", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-ExtraLight.ttf]])
+LSM:Register("font", "Mukta-Light", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-Light.ttf]])
+LSM:Register("font", "Mukta-Medium", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-Medium.ttf]])
+LSM:Register("font", "Mukta-Regular", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-Regular.ttf]])
+LSM:Register("font", "Mukta-SemiBold", [[Interface\AddOns\NoobTacoUI-Media\Media\Fonts\Mukta-SemiBold.ttf]])
